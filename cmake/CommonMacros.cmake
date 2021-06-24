258b4768c8bd328a215148c81a5e3dac852e5754
Zhengheng Li <zhenghenge@gmail.com>
2021-06-24 10:27:43 +0800

    configure pylon libraries

--- cmake/CommonMacros.cmake
+++ cmake/CommonMacros.cmake
@@ -1,5 +1,5 @@
 function(df_standard_exe target_name)
-    set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
+    set(multiValueArgs INCLUDE_DIRS DEFINITIONS LINK_DIRS LIBRARIES)
     cmake_parse_arguments(ARG "" "" "${multiValueArgs}" ${ARGN})
 
     file(GLOB sources src/*)
@@ -8,6 +8,7 @@ function(df_standard_exe target_name)
 
     target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
     target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
+    target_link_directories(${target_name} PRIVATE ${ARG_LINK_DIRS})
     target_link_libraries(${target_name} PRIVATE ${ARG_LIBRARIES})
 
     install(TARGETS ${target_name} DESTINATION bin)
@@ -15,7 +16,7 @@ endfunction()
 
 function(df_standard_lib target_name)
     set(oneValueArgs ALIAS)
-    set(multiValueArgs INCLUDE_DIRS DEFINITIONS LIBRARIES)
+    set(multiValueArgs INCLUDE_DIRS DEFINITIONS LINK_DIRS LIBRARIES)
     cmake_parse_arguments(ARG "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     file(GLOB sources src/*)
@@ -26,6 +27,7 @@ function(df_standard_lib target_name)
     target_include_directories(${target_name} PUBLIC  pub)
     target_include_directories(${target_name} PRIVATE inc ${ARG_INCLUDE_DIRS})
     target_compile_definitions(${target_name} PRIVATE ${ARG_DEFINITIONS})
+    target_link_directories(${target_name} PRIVATE ${ARG_LINK_DIRS})
     target_link_libraries(${target_name} PUBLIC ${ARG_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 
     if(DEFINED ARG_ALIAS)
--- demo/src/demo.cc
+++ demo/src/demo.cc
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <pylon/PylonIncludes.h>
 
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    cout << "demo" << endl;
+    cout << "before init" << endl;
+
+    Pylon::PylonAutoInitTerm autoInitTerm;
+
+    cout << "after init" << endl;
 
     return 0;
 }
